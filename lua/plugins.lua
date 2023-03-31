1e887f45029465e24b3f7e91a35f23a6b18e0d46
Kurtis Moxley <image1224@outlook.com>
2023-03-31 12:12:29 +0800

    Update plugin manager to Lazy.nvim.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,36 +1,34 @@
 local fn = vim.fn
 
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({
-        'git', 'clone', '--depth', '1',
-        'https://github.com/wbthomason/packer.nvim', install_path
-    })
-    vim.cmd 'packadd packer.nvim'
+local lazypath = fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "git@github.com:folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function()
-
-    -- Plugin self
-    use 'wbthomason/packer.nvim'
+return require("lazy").setup({
 
     -- Languages plugins
-    use {
+    {
         'nvim-treesitter/nvim-treesitter',
-        requires = {
+        dependencies = {
             'nvim-treesitter/nvim-treesitter-refactor',
             'nvim-treesitter/nvim-treesitter-textobjects'
         },
-        run = ':TSUpdate'
-    }
-    use 'folke/lua-dev.nvim'
+        build = ':TSUpdate'
+    } ,
+    'folke/lua-dev.nvim' ,
 
     -- Built-in LSP
-    use 'neovim/nvim-lspconfig'
-    use 'williamboman/nvim-lsp-installer'
+    'neovim/nvim-lspconfig' ,
+    'williamboman/nvim-lsp-installer' ,
 
     -- [[ 
     --      Coc.nvim, alternative for Neovim's built-in LSP. 
@@ -40,53 +38,53 @@ return require('packer').startup(function()
 
     -- Code completion
     -- nvim-cmp
-    use 'hrsh7th/cmp-nvim-lsp' -- { name = nvim_lsp }
-    use 'hrsh7th/cmp-buffer'   -- { name = 'buffer' },
-    use 'hrsh7th/cmp-path'     -- { name = 'path' }
-    use 'hrsh7th/cmp-cmdline'  -- { name = 'cmdline' }
-    use 'hrsh7th/nvim-cmp'
+    'hrsh7th/cmp-nvim-lsp' , -- { name = nvim_lsp }
+    'hrsh7th/cmp-buffer' ,   
+    'hrsh7th/cmp-path' ,         
+    'hrsh7th/cmp-cmdline' ,  
+    'hrsh7th/nvim-cmp' ,
     -- vsnip
-    use 'hrsh7th/cmp-vsnip'    -- { name = 'vsnip' }
-    use 'hrsh7th/vim-vsnip'
-    use 'rafamadriz/friendly-snippets'
+    'hrsh7th/cmp-vsnip' ,    
+    'hrsh7th/vim-vsnip' ,
+    'rafamadriz/friendly-snippets' ,
     -- lspkind
-    use 'onsails/lspkind-nvim'
+    'onsails/lspkind-nvim' ,
 
-    use 'rust-lang/rust.vim'
+    'rust-lang/rust.vim' ,
 
-    use 'elixir-lang/vim-elixir'
-    use 'thinca/vim-ref'
-    use 'slashmili/alchemist.vim'
+    'elixir-lang/vim-elixir' ,
+    'thinca/vim-ref' ,
+    'slashmili/alchemist.vim' ,
 
-    use 'vim-ruby/vim-ruby'
+    'vim-ruby/vim-ruby' ,
 
-    use 'vim-erlang/vim-erlang-runtime'
-    use 'vim-erlang/vim-erlang-omnicomplete'
-    use 'vim-erlang/vim-erlang-tags'
-    use 'vim-erlang/vim-erlang-compiler'
+    'vim-erlang/vim-erlang-runtime' ,
+    'vim-erlang/vim-erlang-omnicomplete' ,
+    'vim-erlang/vim-erlang-tags' ,
+    'vim-erlang/vim-erlang-compiler' ,
 
-    use 'derekwyatt/vim-scala'
+    'derekwyatt/vim-scala' ,
 
     -- Markdown
-    use {
+    {
         'iamcco/markdown-preview.nvim',
-        run = 'cd app & yarn install' 
-    }
+        build = 'cd app & yarn install' 
+    } ,
 
     -- Files
-    use 'preservim/nerdtree'
-    use 'ryanoasis/vim-devicons'
-    use 'Chiel92/vim-autoformat'
-    -- use 'dense-analysis/ale'
-    use 'jiangmiao/auto-pairs'
-    use 'sbdchd/neoformat'
+    'preservim/nerdtree' ,
+    'ryanoasis/vim-devicons' ,
+    'Chiel92/vim-autoformat' ,
+    -- 'dense-analysis/ale' ,
+    'jiangmiao/auto-pairs' ,
+    'sbdchd/neoformat' ,
 
     -- Asynchronous terminal emulator 
-    use 'akinsho/toggleterm.nvim'
+    'akinsho/toggleterm.nvim' ,
 
     --Color schemes
-    use 'fxn/vim-monochrome'
-    use 'fenetikm/falcon'
-    use 'rafi/awesome-vim-colorschemes'
+    'fxn/vim-monochrome' ,
+    'fenetikm/falcon' ,
+    'rafi/awesome-vim-colorschemes' ,
 
-end)
+})
