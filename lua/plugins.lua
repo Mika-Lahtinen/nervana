a1fdd6436fa831e82283ee98be867144d1563215
Kurtis Moxley <image1224@outlook.com>
2023-08-02 11:27:24 +0800

    Format codes.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,14 +1,14 @@
 vim.cmd [[packadd packer.nvim]]
 
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -21,46 +21,49 @@ vim.cmd([[
 ]])
 
 return require('packer').startup({
-  function(use)
-    use "wbthomason/packer.nvim"
-    use {"folke/which-key.nvim"}
-    use {"nvim-treesitter/nvim-treesitter", 
-          run = function()
-            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-            ts_update()
-          end,
+    function(use)
+        use "wbthomason/packer.nvim"
+        use {"folke/which-key.nvim"}
+        use {
+            "nvim-treesitter/nvim-treesitter",
+            run = function()
+                local ts_update = require('nvim-treesitter.install').update({
+                    with_sync = true
+                })
+                ts_update()
+            end
         }
 
-    use {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      "neovim/nvim-lspconfig",
+        use {"williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim", "neovim/nvim-lspconfig"}
+        use "hrsh7th/cmp-nvim-lsp"
+        use "hrsh7th/cmp-buffer"
+        use "hrsh7th/cmp-path"
+        use "hrsh7th/cmp-cmdline"
+        use "hrsh7th/nvim-cmp"
+
+        use {"hrsh7th/vim-vsnip", "hrsh7th/vim-vsnip-integ", "rafamadriz/friendly-snippets", "hrsh7th/cmp-vsnip"}
+
+        use {
+            'nvim-orgmode/orgmode',
+            config = function()
+                require('orgmode').setup {}
+            end
+        }
+
+        if packer_bootstrap then
+            require('packer').sync()
+        end
+    end,
+
+    config = {
+        max_jobs = 16,
+        display = {
+            open_fn = function()
+                return require('packer.util').float({
+                    border = 'single'
+                })
+            end
+        }
     }
-    use "hrsh7th/cmp-nvim-lsp"
-    use "hrsh7th/cmp-buffer"
-    use "hrsh7th/cmp-path"
-    use "hrsh7th/cmp-cmdline"
-    use "hrsh7th/nvim-cmp"
-
-    use {
-      "hrsh7th/vim-vsnip",
-      "hrsh7th/vim-vsnip-integ",
-      "rafamadriz/friendly-snippets",
-      "hrsh7th/cmp-vsnip",
-    }
-
-    if packer_bootstrap then
-    	require('packer').sync()
-    end
-  end,
-
-  config = {
-		max_jobs = 16,
-		display = {
-			open_fn = function()
-				return require('packer.util').float({ border = 'single' })
-			end
-		}
-	}
 
 })
